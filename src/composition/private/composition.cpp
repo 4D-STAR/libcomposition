0f68f2959e2d526356d6c5955604417fad44552c
Emily Boudreaux <thomas@boudreauxmail.com>
2025-06-19 13:23:31 -0400

    feat(network): major progress on network finalization and matrix creation

--- src/composition/private/composition.cpp
+++ src/composition/private/composition.cpp
@@ -19,6 +19,7 @@
 //
 // *********************************************************************** */
 #include "composition.h"
+#include "const.h"
 #include "quill/LogMacros.h"
 
 #include <stdexcept>
